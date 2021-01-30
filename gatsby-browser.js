5a41f48330cbb36a9866d74c7b4394dee09ec917
Logan Takahashi <logan.takahashi@gmail.com>
2021-01-30 15:55:09 -0500

    gatsby browser config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,23 @@
  */
 
 // You can delete this file if you're not using it
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import {
+  faChevronDown,
+  faChevronRight,
+  faChevronLeft,
+  faArrowRight,
+  faArrowLeft,
+} from "@fortawesome/free-solid-svg-icons"
+
+export function onClientEntry() {
+  library.add(
+    fab,
+    faChevronDown,
+    faChevronRight,
+    faChevronLeft,
+    faArrowRight,
+    faArrowLeft
+  )
+}
